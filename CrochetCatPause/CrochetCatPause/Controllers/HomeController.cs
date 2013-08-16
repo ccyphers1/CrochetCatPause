385d134222dd619f9d5c10a6960e78b4c5ed5c51
ccyphers1 <ccyphers1@wccnet.edu>
2013-08-16 17:45:55 -0400

    Contact page and function

--- CrochetCatPause/CrochetCatPause/Controllers/HomeController.cs
+++ CrochetCatPause/CrochetCatPause/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using CrochetCatPause.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -35,5 +36,37 @@ namespace CrochetCatPause.Controllers
             return View();
         }
 
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactMessage message)
+        {
+            if (!ModelState.IsValid) return RedirectToAction("Listen");
+
+            MailMessage mail = new MailMessage();
+            SmtpClient client = new SmtpClient();
+
+            mail.From = new MailAddress("crochetcatpausesite@gmail.com", "CCP Website");
+            mail.To.Add(new MailAddress("chancecyphers@gmail.com"));
+            if (message.Subject == null) mail.Subject = "Too cool for a subject";
+            mail.Subject = message.Subject;
+            mail.Body = message.Body + Environment.NewLine + "Return Email: " + message.ReturnEmail;
+
+            client.Host = "smtp.gmail.com";
+            client.Port = 587;
+            client.Credentials = new System.Net.NetworkCredential("crochetcatpausesite@gmail.com", "ultimatepassword");
+            client.EnableSsl = true;
+            client.Send(mail);
+
+            return RedirectToAction("Thanks");
+        }
+
+        public ActionResult Thanks()
+        {
+            return View();
+        }
     }
 }
---
+++ CrochetCatPause/CrochetCatPause/Models/ContactMessage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CrochetCatPause.Models
+{
+    public class ContactMessage
+    {
+        [Required(ErrorMessage = "Gotta give yur name")]
+        [StringLength(32)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Emails are needed")]
+        [EmailAddress]
+        public string ReturnEmail { get; set; }
+
+        [StringLength(32)]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "You gotta say something...")]
+        [StringLength(1048)]
+        [DataType(DataType.MultilineText)]
+        public string Body { get; set; }
+    }
+}
